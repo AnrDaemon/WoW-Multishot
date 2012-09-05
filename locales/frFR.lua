1198a658d7a37066357b12704ef738c4f434d4c9
dridzt <dridzt@mail.com>
2012-09-06 00:54:07 +0300

    Localization changes (use english for missing terms)
    - Include references to itIT and ptBR

--- locales/frFR.lua
+++ locales/frFR.lua
@@ -1,4 +1,4 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("Multishot", "frFR")
 if not L then return end
 
---@localization(locale="frFR", format="lua_additive_table", handle-subnamespaces="concat")@
+--@localization(locale="frFR", format="lua_additive_table", handle-subnamespaces="concat", handle-unlocalized="english")@
